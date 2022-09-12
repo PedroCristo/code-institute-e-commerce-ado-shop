b83ced9d2acf5ebc13c533d3ed05898e3170c856
Pedro Cristo <pcristo35@gmail.com>
2022-09-12 08:55:35 +0000

    Set debug mode to false

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['code-institute-ado-shop.herokuapp.com', 'localhost']
 
